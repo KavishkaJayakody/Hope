c82e740b4d48dc02f5459826e4e8753ebda358a7
Nilakna Warushavithana <132770745+NilaknaW@users.noreply.github.com>
2025-04-04 16:19:05 +0530

    moved back

---
+++ Hope-raspi/raspi.h
@@ -0,0 +1,25 @@
+#ifndef RASPI_H
+#define RASPI_H
+
+class raspi
+{
+public:
+    raspi(); // Constructor
+    void takePotato(); // Ask Raspberry Pi to take the potato
+    bool isRedGood(); // read the april tag and return true if red is good, return false is blue is good
+    bool isBoxRed(); // find the box colout=r. return true if red, false if blue
+    void openGate(float GOODORBAD); // Open the gate
+    
+    bool detectDryPot(); // Detect dry potato. return true if dry, false if wet
+    void takeWater(); // Ask Raspberry Pi to take water
+    void waterPot(); // Ask Raspberry Pi to water the potato
+    void playStarman(); // Play Starman song
+
+    void ledOn(); // Turn on the LED
+    void ledOff(); // Turn off the LED
+
+    int boxColumnColors(); // Get the box colors of the column
+    void takeBox(); // Ask Raspberry Pi to take the box
+};
+
+#endif // RASPI_H
