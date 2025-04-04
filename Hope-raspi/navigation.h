165988c3be95a026a1ce1808bac8b1ad6d9dcf6a
Nilakna Warushavithana <132770745+NilaknaW@users.noreply.github.com>
2025-04-04 09:45:04 +0530

    task 3

--- Hope-raspi/navigation.h
+++ Hope-raspi/navigation.h
@@ -1,21 +1,28 @@
 #ifndef NAVIGATION_H
 #define NAVIGATION_H
 
+#include "encoders.h"
+
 class navigation
 {
 private:
     /* data */
 public:
     navigation(/* args */);
+    encoders enc;
     // ~navigation();
     void move_straight(float dist_mm); // distance in mm, forward +, backward -
     void turn(float angle_deg); // angle degree, anticlkwise +
-    bool move_straight_till_wall_or_line();
-    bool moveTillWall(float dist_mm); // distance in mm, forward +, backward -
+    void moveTillWall();
+    bool moveTillWallTask2(float currentDist); // distance in mm, forward +, backward -
+    void moveTillLine();
 };
 
-bool navigation::moveTillWall(float task2Dist)
-{
+bool navigation::moveTillWallTask2(float task2Dist)
+{   
+    // update this from time to time and check the condition.
+    float temp = enc.robotDistance(); // Get the distance from the encoders
+    task2Dist = task2Dist + enc.robotDistance() - temp; // Calculate the distance travelled
     if (task2Dist>1200){
         return true;
     } else {
--- Hope-raspi/tasks.h
+++ Hope-raspi/tasks.h
@@ -5,6 +5,9 @@
 #include "navigation.h"
 #include "encoders.h"
 
+# define GOOD 1
+# define BAD 0
+
 class tasks
 {
 public:
@@ -12,8 +15,9 @@ public:
     encoders enc; // Encoders object
     tasks(); // Constructor with navigation object;
     bool task1();
-    bool task2();
-    bool task3();
+    bool task2(); // IMPLEMENTED
+    bool task2nodist(); // just in case...
+    bool task3(); // IMPLEMENTED
     bool task4();
     bool task5();
     bool task6();
@@ -31,8 +35,9 @@ void tasks::task1()
 
 bool tasks::task2()
 {
-    bool wallRoadDone = false;
-    bool foundWall = false;
+    bool wallDone = false;
+    float side = 1; // right line (should turn left)
+    
     float startDist = enc.robotDistance(); // Get the initial distance from the encoders
     float turnDist = 0; // Initialize the end distance
     float tempDist; // Initialize the temporary distance
@@ -42,70 +47,161 @@ bool tasks::task2()
     nav.turn(-90); // Turn 90 degrees clockwise
 
     // WITH DISTANCE MEASUREMENT BUT CANNOT CHECK TASK 2 END =(. NOW CAN. YAY.
-    while (!wallRoadDone) {
-        wallRoadDone = nav.moveTillWall(turnDist); // Move straight until wall or line is found
-        if (wallRoadDone) {
+    while (!wallDone) {
+        
+        // MOVE TILL THE RIGHT WALL
+        wallDone = nav.moveTillWallTask2(turnDist); // Move straight until wall or line is found
+        if (wallDone) {
             break;
         }
         task2Dist = enc.robotDistance() - startDist - turnDist; // Calculate the distance travelled
-        if (found_wall) {
+        
+
+
+        // CHANGE TO LEFT SIDE
+
+        // if (foundWall) {
+        // turn left
+        nav.turn(90); // Turn 90 degrees anticlockwise 
+        
+        //  move forward
+        tempDist = enc.robotDistance();
+        nav.move_straight(300); // Move straight for 150 mm
+        turnDist = turnDist + (enc.robotDistance() - tempDist);
+        
+        //turn right to original direction
+        nav.turn(-90); // Turn 90 degrees clockwise
+        
+
+        
+
+        // MOVE TILL THE LEFT WALL
+        wallDone = nav.moveTillWallTask2(turnDist); // Move straight until wall or line is found
+        if (wallDone) {
+            side = -1; // Set wall to true
+            break; // Exit the loop if task 2 is done
+        }
+
+
+
+        // CHANGE TO RIGHT SIDE
+        // turn right
+        nav.turn(-90); // Turn 90 degrees clockwise
+        
+        // move forward
+        tempDist = enc.robotDistance();
+        nav.move_straight(300); // Move straight for 150 mm
+        turnDist = turnDist + enc.robotDistance() - tempDist;
+        
+        // turn left to original direction
+        nav.turn(90); // Turn 90 degrees clockwise
+        // }
+    }
+
+    // LETS GO RAMP!!!!
+
+    // turn right if on left line.
+    turn((float)90*side); // Turn 90 degrees clockwise if on left line
+    nav.move_straight(150); // Move straight for 150 mm
+    turn((float)(-90)*side);
+    nav.moveTillLine();
+    task2_done = true; // Set task 2 done to true
+
+    return task2_done; // Return the task 2 status
+}
+
+
+bool tasks::task2nodist()
+{
+    // Task 2 implementation without distance measurement
+    nav.move_straight(150); // Move straight for 150 mm
+    nav.turn(-90); // Turn 90 degrees clockwise
+    bool foundLine = false; // Initialize foundLine to false
+
+    while (!task2_done) {
+        // CANNOT FIND TASK 2 DONEEEEEEEEEEEEEEEEEEEEEE
+        foundLine = nav.moveTillWallorLine(); // Move straight until wall or line is found
+        if (!foundLine) {
             // turn left
             nav.turn(90); // Turn 90 degrees anticlockwise 
-            
             //  move forward
-            tempDist = enc.robotDistance();
             nav.move_straight(300); // Move straight for 150 mm
-            turnDist = turnDist + (enc.robotDistance() - tempDist);
-            
             //turn right to original direction
             nav.turn(-90); // Turn 90 degrees clockwise
-            
             // move again till left wall found
-            wallRoadDone = nav.moveTillWall(turnDist); // Move straight until wall or line is found
-            if (wallRoadDone) {
+            foundLine = nav.moveTillWallorLine();
+            if (!foundLine) {
+                task2_done = true; // Set task 2 done to true
                 break; // Exit the loop if task 2 is done
             }
-
             // turn right
             nav.turn(-90); // Turn 90 degrees clockwise
-            
             // move forward
-            tempDist = enc.robotDistance();
             nav.move_straight(300); // Move straight for 150 mm
-            turnDist = turnDist + enc.robotDistance() - tempDist;
-            
             // turn left to original direction
             nav.turn(90); // Turn 90 degrees clockwise
         }
+        if (!foundLine) {
+            task2_done = true; // Set task 2 done to true
+            break; // Exit the loop if task 2 is done
+        }
     }
-
-    // while (!task2_done) {
-    //     // CANNOT FIND TASK 2 DONEEEEEEEEEEEEEEEEEEEEEE
-    //     found_wall = nav.moveTillWall(); // Move straight until wall or line is found
-    //     if (found_wall) {
-    //         // turn left
-    //         nav.turn(90); // Turn 90 degrees anticlockwise 
-    //         //  move forward
-    //         nav.move_straight(300); // Move straight for 150 mm
-    //         //turn right to original direction
-    //         nav.turn(-90); // Turn 90 degrees clockwise
-    //         // move again till left wall found
-    //         nav.moveTillWall();
-    //         // turn right
-    //         nav.turn(-90); // Turn 90 degrees clockwise
-    //         // move forward
-    //         nav.move_straight(300); // Move straight for 150 mm
-    //         // turn left to original direction
-    //         nav.turn(90); // Turn 90 degrees clockwise
-    //     }
-    // }
     return task2_done; // Return the task 2 status
 }
 
 
 void tasks::task3()
-{
-    // Task 3 implementation
+{   
+    // READ APRILTAG
+   
+    nav.moveTillWall();
+    nav.turn(-90); // Turn 90 degrees clockwise
+    nav.moveTillLine();
+    nav.turn(90);
+    nav.moveTillJunction(); // align also
+    nav.move_straight(-150);
+    nav.turn(-90);
+    bool goodRed = raspi.isRedGood(); // ask raspberry to find tag
+
+    // BASKETING POTATOES 
+
+    // go to the basket and read tag
+    nav.move_straight(-100); // reverse a bit
+    nav.turn(-90);
+    nav.moveTillLine(); // align also
+    // nav.move_straight(300);
+    nav.move_straight(-150);
+    nav.turn(90); // turn towards the basket to read
+    bool redBox = raspi.findBoxColour(); // ask raspberry to find tag
+
+    // turn rear to put the potatoes
+    nav.turn(180);
+    nav.move_straight(-50);
+    
+    bool openGood = false;
+
+    if ((goodRed && redBox)||(!goodRed && !redBox)) {
+        servo.openGate(GOOD); //ASK OSHANI ABOUT SERVO CONTROLLING
+        openGood = true;
+    } else {
+        // goodRed && !redBox || !goodRed && redBox
+        servo.openGate(BAD);
+    }
+
+    // go to the next basket
+    nav.move_straight(50);
+    nav.turn(-90);
+    nav.moveTillLine(); // and align
+
+    nav.move_straight(-150); // move towards the box
+    nav.turn(90); // turn rear towards the box
+
+    // put the other set of potatoes
+    if (openGood) {
+        servo.openGate(BAD);
+    } else {
+        servo.openGate(GOOD);
+    }
 }
 
 void tasks::task4()
